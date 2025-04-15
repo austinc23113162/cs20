67fa348b1c0f5c5071e5f44e55a31d7a969c117e
Austin Chang <austin.chang@tufts.edu>
2025-04-15 12:52:12 -0400

    Update node_heroku.js

--- node_heroku.js
+++ node_heroku.js
@@ -5,11 +5,11 @@ var port = process.env.PORT || 3000;
 console.log("This goes to the console window");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  urlObj = url.parse(req.url,true)
-  id = urlObj.query.id
-   res.write ("Success!  This app is deployed online");
+  //urlObj = url.parse(req.url,true)
+  //id = urlObj.query.id
+  res.write ("Success!  This app is deployed online");
   res.write("<h2>This is my hello application</h2>");
-  res.write ("The id is: " + id)
+  //res.write ("The id is: " + id)
   res.end();
   console.log('hey')
 }).listen(port);
